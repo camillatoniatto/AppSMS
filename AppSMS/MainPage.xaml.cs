e5bcc16f96a56b204c1a437da68478f27d35014a
Camiss <camillatoniatto@gmail.com>
2022-11-26 11:12:42 -0300

    Adicionar arquivos de projeto.

---
+++ AppSMS/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+namespace XF_SMS1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public async Task EnviaSms(string body, string recipient)
+        {
+            try
+            {
+                var sms = new SmsMessage(body, recipient);
+                await Sms.ComposeAsync(sms);
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Ops...", "Essa função não é suportada em seu dispositivo.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void btnEnviaSms_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtNumero.Text))
+            {
+                await EnviaSms(txtMensagem.Text, txtNumero.Text);
+            }
+        }
+    }
+}
